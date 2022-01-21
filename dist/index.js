acc8207d73fa0237277c6d1677f2afb15acf74bf
Nathan Staab <nstaab443@gmail.com>
2022-01-21 17:22:31 -0500

    Delete dist directory

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const proxy_1 = require("./proxy/proxy");
-// const singletonPattern = new SingletonPattern();
-const proxyTest = new proxy_1.ProxyTest();
